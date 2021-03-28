db5aa6ed22ad6f52c008d5a50cf753a6cb48db0d
Vepathakpawan <pawanved007@gmail.com>
2021-03-28 08:05:16 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{UserComponent} from './user/user.component'; 
+import{UserdetailsComponent} from './userdetails/userdetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'user',
+  component: UserComponent
+},
+{
+  path:'userdetails',
+  component:UserdetailsComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserdetailsComponent,
   ],
   imports: [
     BrowserModule,
